b78bf0702a077dc5eaabc417fe4f7102cf6e80ba
Benjamin <carlosbpac@gmail.com>
2025-10-30 09:35:08 -0600

    fix: change TemplateRepository to use UUID instead of String for entity ID

--- src/main/java/com/sa/notification_service/templates/infrastructure/persistenceadapter/repositories/TemplateRepository.java
+++ src/main/java/com/sa/notification_service/templates/infrastructure/persistenceadapter/repositories/TemplateRepository.java
@@ -1,11 +1,12 @@
 package com.sa.notification_service.templates.infrastructure.persistenceadapter.repositories;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sa.notification_service.templates.infrastructure.persistenceadapter.models.TemplateEntity;
 
-public interface TemplateRepository extends JpaRepository<TemplateEntity, String> {
+public interface TemplateRepository extends JpaRepository<TemplateEntity, UUID> {
     public Optional<TemplateEntity> findOneByCode(String code);
 }
